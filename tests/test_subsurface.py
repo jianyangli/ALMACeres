6651a0c315c36428a7b20c85b65f12f12e88829b
Jian-Yang Li <jianyang2li@gmail.com>
2019-01-29 17:17:14 -0500

    update test code for subsurface emission model

--- tests/test_subsurface.py
+++ tests/test_subsurface.py
@@ -3,7 +3,7 @@
 # 5/11/2017, JYL @PSI
 
 import numpy
-from ..core import surface
+from ALMACeres.core import Surface, Layer, loss_tangent
 
 # Some tests
 if __name__ == '__main__':
@@ -11,8 +11,11 @@ if __name__ == '__main__':
     from matplotlib import pyplot as plt
     from matplotlib.backends.backend_pdf import PdfPages
     from jylipy import pplot
+    import os
 
-    pdf = PdfPages('subsurface_emission_test.pdf')
+    workdir = os.path.dirname(__file__)
+
+    pdf = PdfPages(os.path.join(workdir,'subsurface_emission_test.pdf'))
 
     # temperature profile
     zz = numpy.linspace(0,300,1000)
@@ -22,7 +25,7 @@ if __name__ == '__main__':
     z = numpy.linspace(0,500,1000)
     plt.plot(z,T(z))
     pplot(xlabel='z-depth',ylabel='T * Emissivity')
-    pdf.savefig()
+    #pdf.savefig()
 
     # optical constants
     n = numpy.sqrt(3)  # refractive index
@@ -30,9 +33,10 @@ if __name__ == '__main__':
     # brightness temperature as a function of emissiona angle
     loss = 0.0067   # loss tangent
     wavelength = 10.   # wavelength
-    surf = surface(T, n, loss, emissivity=1.0)
+    layer = Layer(n, loss, emissivity=1.0, profile=T)
+    surf = Surface(layer)
     emi = numpy.linspace(0,90,100)
-    Tb = surf.Tb(emi, wavelength)
+    Tb = numpy.array([surf.emission(ee, wavelength) for ee in emi])
     plt.clf()
     plt.plot(emi, Tb)
     pplot(xlabel='Emission Angle (deg)',ylabel='Tb')
@@ -44,9 +48,9 @@ if __name__ == '__main__':
     Tb = []
     leng = []
     for ls in loss:
-        surf = surface(T, n, ls, emissivity=1.0)
-        Tb.append(surf.Tb(0, wavelength))
-        leng.append(surf.absorption_length())
+        surf.layers[0].loss_tangent = ls
+        Tb.append(surf.emission(0, wavelength))
+        leng.append(surf.layers[0].absorption_length(wavelength))
     plt.clf()
     plt.plot(loss, Tb)
     pplot(xscl='log',xlabel='Loss Tangent',ylabel='Tb')
@@ -55,8 +59,9 @@ if __name__ == '__main__':
     # brightness temperature as a function of wavelength
     loss = 0.0067  # loss tangent
     wavelength = numpy.logspace(-2,3.5,100)  # wavelength
-    surf = surface(T, n, loss, emissivity=1.0)
-    Tb = [surf.Tb(0, wavelength=x) for x in wavelength]
+    layer = Layer(n, loss, emissivity=1.0, profile=T)
+    surf = Surface(layer)
+    Tb = [surf.emission(0, wavelength=x) for x in wavelength]
     plt.clf()
     plt.plot(wavelength, Tb)
     pplot(xscl='log',xlabel='Wavelength',ylabel='Tb')
@@ -67,7 +72,7 @@ if __name__ == '__main__':
 
     # test the effects of refractive index and absorption length on effective
     # brightness temperature
-    pdf = PdfPages('test_Le_n.pdf')
+    pdf = PdfPages(os.path.join(workdir,'test_Le_n.pdf'))
 
     # assumed temperature profile
     zz = numpy.linspace(0,300,1000)
@@ -84,28 +89,38 @@ if __name__ == '__main__':
 
     # test effect of absorption length Le
     n = 1.5
+    wavelength = 10.
     Le = numpy.logspace(-1,4,100)
     Tb = []
+    loss_s = []
+    layer = Layer(n, loss, profile=T)
+    surf = Surface(layer)
     for l in Le:
-        surf = surface(T, n, 0., absorption_length=l)
-        Tb.append(surf.Tb(0., wavelength))
+        loss = loss_tangent(n, l, wavelength)
+        loss_s.append(loss)
+        surf.layers[0].loss_tangent = loss
+        Tb.append(surf.emission(0., wavelength))
     plt.clf()
-    plt.plot(Le/wavelength, Tb)
+    plt.plot(Le, Tb)
     pplot(xlabel='Absorption Length (wavelength)',ylabel='Tb (K, for T$_0$=300 K)',xscl='log',title='n={0}'.format(n))
     pdf.savefig()
 
     # test effect of refractive index
-    Le = 10
-    rn = numpy.linspace(1.,3,20)
-    emi = numpy.linspace(0,89.9,20)
+    loss = 0.0067   # loss tangent
+    wavelength = 10.   # wavelength
+    rn = numpy.linspace(1.,3,5)
+    emi = numpy.linspace(0,89.9,30)
     Tb = []
+    layer = Layer(n, loss, profile=T)
+    surf = Surface(layer)
     for n in rn:
-        surf = surface(T, n, 0., absorption_length=Le)
-        Tb.append(surf.Tb(emi, wavelength))
+        surf.layers[0].n = n
+        Tb.append([surf.emission(ee, wavelength) for ee in emi])
     Tb = numpy.array(Tb).T
     plt.clf()
     plt.plot(emi, Tb)
-    pplot(xlabel='Emission Angle (deg)', ylabel='Tb', title='n=1 to 3, Le/wavelength=1')
+    pplot(xlabel='Emission Angle (deg)', ylabel='Tb (K)', title=r'$\tan(\Delta)=0.0067$, $\lambda=10$')
+    plt.legend([f'n = {i:.2f}' for i in rn])
     pdf.savefig()
 
     pdf.close()
