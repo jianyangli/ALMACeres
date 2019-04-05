fix calculation of subsurface emission

core.py
@@ -484,7 +484,6 @@ class Layer(object):
         self.n = n
         self.depth = depth
         self.loss_tangent = loss_tangent
-        self.emissivity = emissivity
         self.profile = profile
 
     def absorption_length(self, *args, **kwargs):
@@ -502,8 +501,8 @@ class Layer(object):
         wave_freq : number, `astropy.units.Quantity` or array_like
             Wavelength or frequency of observation
         """
-        if isinstance(wave_freq, u.Quantity):
-            wavelength = wave_freq.to(u.m, equivalencies=u.spectral())
+        if isinstance(wave_freq, units.Quantity):
+            wavelength = wave_freq.to(units.m, equivalencies=units.spectral())
         else:
             wavelength = wave_freq
         c = np.sqrt(1+self.loss_tangent*self.loss_tangent)
@@ -572,7 +571,9 @@ class Surface(object):
             prof = {'t': [], 'intprofile': [], 'zzz': [], 'L0': []}
         for i,l in enumerate(self.layers):
             # integrate in layer `l`
-            inc = Snell(l.n, n0).angle1(emi_ang)
+            snell = Snell(l.n, n0)
+            inc = snell.angle1(emi_ang)
+            ref_coef = snell.reflectance_coefficient(angle2=emi_ang)
             coef = l.absorption_coefficient(wavelength)
             cos_i = np.cos(np.deg2rad(inc))
             if debug:
@@ -590,10 +591,10 @@ class Surface(object):
                 D += l.depth
             from scipy.integrate import quad
             integral = quad(intfunc, 0, l.depth, epsrel=epsrel)[0]
-            m += l.emissivity*coef*integral/cos_i
+            m += (1-ref_coef)*coef*integral/cos_i
             # prepare for the next layer
             L += l.depth/cos_i*coef
-            emi_ang = np.arccos(cos_i)
+            emi_ang = inc
             n0 = l.n
 
         if debug: